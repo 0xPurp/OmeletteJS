e3db7a41cb209b83e0a7bcda45655a38d87ae752
0xPurp <haroune1020@hotmail.com>
2021-05-25 12:10:42 +0200

    Fin class Personnage ajout fonctions (acheter, vider, viderBol)

--- src/js/classes.js
+++ src/js/classes.js
@@ -36,6 +36,27 @@ class Personne {
                 console.log(`${this.nom} as rendu son panier`)
             }
         }
+        this.acheter = (endroit) => {
+            if (endroit.nom == "Epicerie") {
+                endroit.ingredients.forEach(element => {
+                    this.mainDroite[0].contenu.push(element);
+                    this.argent -= element.prix;
+                    console.log(`${this.nom} avez acheter ${element.nom} à ${element.prix}€`);
+                });
+            }else {
+                console.log(`${this.nom} n'est pas dans une Epicerie.`);
+            }
+        }
+        this.vider = () => {
+            while(this.mainDroite[0].contenu.length > 0){
+                console.log(`${this.nom} avez ajouté ${this.mainDroite[0].contenu[0].nom}`);
+                bol.contenu.push(this.mainDroite[0].contenu.shift());
+            }
+        }
+        this.viderBol = () => {
+            poele.contenu.push(bol.contenu.shift());
+            console.log(`${this.nom} vide le bol dans la poele`);
+        }
     }
 }
 
